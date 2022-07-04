dc998bc39d9440526f96fd4eac01561cddfd826f
Dima Nekrasov <dmitrij.nekrasov@gmail.com>
2022-07-04 12:48:05 +0400

    add migrate app

---
+++ app/migrate/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dimashiro/test_mediasoft/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.New(
+		"file://migrations",
+		cfg.GetDBConnString())
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
